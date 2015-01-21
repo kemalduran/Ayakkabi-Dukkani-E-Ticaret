3a008bb6aab218342b6d84b814396a21bad7cca2
kemalduran <kemalduran06@gmail.com>
2015-01-21 12:26:06 +0200

    E-Ticaret Web Sitesi

--- ayakkabidukkani/app_config.php
+++ ayakkabidukkani/app_config.php
@@ -1,5 +1,5 @@
 <?php
     define('DB_NAME', 'ayakkabi_dukkani');
     define('DB_USER', 'root');
-    define('DB_PASSWORD', '10290145');
+    define('DB_PASSWORD', 'your-password');
     define('DB_HOST', 'localhost');
